bf9c70845cfef020998b5f129b09527645bc915e
himani44444 <pihu63112@gmail.com>
2026-08-27 10:07:37 +0000

    Reenable unit test that was disabled

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -7,7 +7,7 @@ import pytest
 
 # Project Modules
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
-from calculations import area_of_circle, get_nth_fibonacci   # noqa: E402
+from calculations import area_of_circle, get_nth_fibonacci  # noqa: E402
 
 
 def test_area_of_circle_positive_radius():
@@ -58,13 +58,13 @@ def test_get_nth_fibonacci_one():
     assert result == 1
 
 
-# def test_get_nth_fibonacci_ten():
-#     """Test with n=10."""
-#     # Arrange
-#     n = 10
+def test_get_nth_fibonacci_ten():
+    """Test with n=10."""
+    # Arrange
+    n = 10
 
-#     # Act
-#     result = get_nth_fibonacci(n)
+    # Act
+    result = get_nth_fibonacci(n)
 
-#     # Assert
-#     assert result == 89
+    # Assert
+    assert result == 89
